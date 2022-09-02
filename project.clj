899785b8e7ef4a625cf928f7128c0a231d65cdd7
BuddhiLW <pedrogbranquinho@gmail.com>
2022-09-02 08:55:14 -0300

    Add ring -- jetty serve

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject stark-challenge "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Stark API integration - generate and process payments"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [ring/ring-core "1.9.5"]
+                 [ring/ring-jetty-adapter "1.9.5"]]
   :main ^:skip-aot stark-challenge.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/stark_challenge/core.clj
+++ src/stark_challenge/core.clj
@@ -1,7 +1,13 @@
 (ns stark-challenge.core
+  (:require [ring.adapter.jetty :as j])
+            ;; [ring.util.response :as resp])
   (:gen-class))
 
+(defn handler [request]
+  {:status 200
+   :headers {"Content-type" "text/pain"}
+   :body "Running on port 3130"})
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (j/run-jetty handler {:port 3130}))
