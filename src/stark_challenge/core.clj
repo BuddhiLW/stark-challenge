03136345101c1e1b492640233a89f446c0adcb7f
BuddhiLW <pedrogbranquinho@gmail.com>
2022-09-02 09:42:50 -0300

    Add starkbank library

--- src/stark_challenge/core.clj
+++ src/stark_challenge/core.clj
@@ -1,7 +1,12 @@
 (ns stark-challenge.core
-  (:require [ring.adapter.jetty :as j])
+  (:require [ring.adapter.jetty :as j]
+            [starkbank.core :as stark])
             ;; [ring.util.response :as resp])
   (:gen-class))
+
+(def key-pair (stark/create "/sample/destination/path"))
+(println (:private-pem key-pair))
+(println (:public-pem key-pair))
 
 (defn handler [request]
   {:status 200
